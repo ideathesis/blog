447f71d27c5eee8f5d998e7cd1a284ff150d1612
ideathesis <ideathesis.id@gmail.com>
2025-02-15 04:10:08 +0700

    update

--- script/search-article.js
+++ script/search-article.js
@@ -94,9 +94,8 @@ document.addEventListener("DOMContentLoaded", () => {
           <div class="result-content">
             <h2 class="result-title">${article.title}</h2>
             <div class="result-meta">
-              <span class="author">Oleh : ${article.author}</span>
-              <time datetime="${article.dateObject.toISOString()}">
-                ${article.date}
+			<span class="author">Oleh: ${article.author}</span><span class="separator"> | </span>
+			<time datetime="${article.dateObject.toISOString()}">${article.date}</time>
               </time>
             </div>
           </div>
